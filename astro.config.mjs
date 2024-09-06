b1ef8b3c57db1322cd89af9bd6dbd3bc7286e9e2
Isaac <it238@byu.edu>
2024-09-05 20:48:25 -0600

    change theme

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,30 +1,4 @@
-import { defineConfig } from "astro/config";
-import tailwind from "@astrojs/tailwind";
-import icon from "astro-icon";
-
-import netlify from "@astrojs/netlify";
-import partytown from "@astrojs/partytown";
-import sitemap from "@astrojs/sitemap";
+import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({
-  site: "https://vinitshahdeo.com",
-  integrations: [
-    tailwind(),
-    icon(),
-    sitemap(),
-    partytown({ config: { forward: ["dataLayer.push"] } }),
-  ],
-  output: "hybrid",
-  adapter: netlify({
-    imageCDN: false,
-  }),
-  vite: {
-    resolve: {
-      alias: {
-        "@styles": "/src/styles",
-      },
-    },
-  },
-  cacheOnDemandPages: true,
-});
+export default defineConfig({});
